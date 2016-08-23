2a1c707af31cef7d331dddc2d689fecd17db2abf
Chris Emerson <chris.emerson@dickiesstore.co.uk>
2016-08-23 14:30:38 +0100

    Add decorator class to handle salted passwords

---
+++ spec/PasswordHashingStrategies/SaltedPasswordHashingStrategyDecoratorSpec.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types = 1);
+
+namespace spec\CEmerson\AceAuth\PasswordHashingStrategies;
+
+use CEmerson\AceAuth\PasswordHashingStrategies\PasswordHashingStrategy;
+use PhpSpec\ObjectBehavior;
+
+class SaltedPasswordHashingStrategyDecoratorSpec extends ObjectBehavior
+{
+    function let(PasswordHashingStrategy $passwordHashingStrategy)
+    {
+        $this->beConstructedWith($passwordHashingStrategy);
+
+        $this->setSalt('test_salt');
+    }
+
+    function it_calls_the_hash_function_of_the_decorated_class_with_the_salted_password(
+        PasswordHashingStrategy $passwordHashingStrategy
+    ) {
+        $passwordHashingStrategy->hashPassword('test_passwordtest_salt')->shouldBeCalled()->willReturn('test_hash');
+
+        $this->hashPassword('test_password')->shouldReturn('test_hash');
+    }
+
+    function it_calls_the_verify_function_of_the_decorated_class_with_the_salted_password(
+        PasswordHashingStrategy $passwordHashingStrategy
+    ) {
+        $passwordHashingStrategy->verifyPassword('test_passwordtest_salt', 'test_hash')->shouldBeCalled()->willReturn(true);
+
+        $this->verifyPassword('test_password', 'test_hash')->shouldReturn(true);
+    }
+}
---
+++ src/PasswordHashingStrategies/SaltedPasswordHashingStrategyDecorator.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types = 1);
+
+namespace CEmerson\AceAuth\PasswordHashingStrategies;
+
+class SaltedPasswordHashingStrategyDecorator implements PasswordHashingStrategy
+{
+    /** @var PasswordHashingStrategy */
+    private $passwordHashingStrategy;
+
+    /** @var string */
+    private $salt;
+
+    public function __construct(PasswordHashingStrategy $passwordHashingStrategy)
+    {
+        $this->passwordHashingStrategy = $passwordHashingStrategy;
+    }
+
+    public function setSalt(string $salt)
+    {
+        $this->salt = $salt;
+    }
+
+    public function hashPassword(string $password): string
+    {
+        return $this->passwordHashingStrategy->hashPassword(
+            $this->getSaltedPassword($password)
+        );
+    }
+
+    public function verifyPassword(string $passwordToVerify, string $passwordHash): bool
+    {
+        return $this->passwordHashingStrategy->verifyPassword(
+            $this->getSaltedPassword($passwordToVerify), $passwordHash
+        );
+    }
+
+    private function getSaltedPassword(string $password): string
+    {
+        return $password . $this->salt;
+    }
+}
