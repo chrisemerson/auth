631ff608460fe573e414b5d8705f9a62dbd754dc
Chris Emerson <chris.emerson@dickiesstore.co.uk>
2016-08-25 11:35:03 +0100

    Test for abstract User class

---
+++ spec/Users/TestUserSpec.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types = 1);
+
+namespace spec\CEmerson\AceAuth\Users;
+
+use CEmerson\AceAuth\PasswordHashingStrategies\PasswordHashingStrategy;
+use PhpSpec\ObjectBehavior;
+
+class TestUserSpec extends ObjectBehavior
+{
+    function let(PasswordHashingStrategy $passwordHashingStrategy)
+    {
+        $this->beConstructedWith($passwordHashingStrategy);
+    }
+
+    function it_uses_the_password_hashing_strategy_to_check_the_users_password(
+        PasswordHashingStrategy $passwordHashingStrategy
+    ) {
+        $passwordHashingStrategy
+            ->verifyPassword('test_password', 'test_password_hash')
+            ->shouldBeCalled()
+            ->willReturn(false);
+
+        $this->verifyPassword('test_password')->shouldReturn(false);
+    }
+}
