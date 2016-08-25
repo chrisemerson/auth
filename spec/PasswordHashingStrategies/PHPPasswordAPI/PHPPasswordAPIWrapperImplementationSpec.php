542e5607024c12e3c29ee74499cbb56fbc2b0165
Chris Emerson <chris.emerson@dickiesstore.co.uk>
2016-08-25 12:05:12 +0100

    End to end test for PHP Password API wrapper

---
+++ spec/PasswordHashingStrategies/PHPPasswordAPI/PHPPasswordAPIWrapperImplementationSpec.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types = 1);
+
+namespace spec\CEmerson\AceAuth\PasswordHashingStrategies\PHPPasswordAPI;
+
+use PhpSpec\ObjectBehavior;
+
+class PHPPasswordAPIWrapperImplementationSpec extends ObjectBehavior
+{
+    function it_hashes_a_password_and_verifies_the_same_password()
+    {
+        $passwordHash = $this->hash('test_password', PASSWORD_DEFAULT);
+
+        $this->verify('test_password', $passwordHash)->shouldReturn(true);
+    }
+
+    function it_fails_verification_of_a_different_password_after_hashing()
+    {
+        $passwordHash = $this->hash('test_password', PASSWORD_DEFAULT);
+
+        $this->verify('wrong_password', $passwordHash)->shouldReturn(false);
+    }
+}
