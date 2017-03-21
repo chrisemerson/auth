4e1aac8ba962b43bd924a1d7809f11834f5a2dce
Chris Emerson <chris.emerson@dickiesstore.co.uk>
2017-03-21 16:44:09 +0000

    Add getter methods back into interface

--- src/RememberedLogins/RememberedLogin.php
+++ src/RememberedLogins/RememberedLogin.php
@@ -2,9 +2,15 @@
 
 namespace CEmerson\Auth\RememberedLogins;
 
+use DateTimeInterface;
+
 interface RememberedLogin
 {
     public function getUsername(): string;
 
+    public function getSelector(): string;
+
     public function getToken(): string;
+
+    public function getExpiryDateTime(): DateTimeInterface;
 }
