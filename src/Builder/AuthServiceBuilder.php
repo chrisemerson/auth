ad876f312167008f43116f4a45979dfcedc9ea44
Chris Emerson <chris.emerson@dickiesstore.co.uk>
2017-03-22 16:07:45 +0000

    Add initial Builder class to help construct an Auth service

---
+++ src/Builder/AuthServiceBuilder.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace CEmerson\Auth\Builder;
+
+use CEmerson\Auth\Auth;
+use CEmerson\Auth\Users\AuthUserGateway;
+
+class AuthServiceBuilder
+{
+    /** @var AuthUserGateway */
+    private $authUserGateway;
+
+    public function __construct()
+    {
+        $this->authUserGateway = null;
+    }
+
+    public function setUserGateway(AuthUserGateway $authUserGateway): self
+    {
+        $this->authUserGateway = $authUserGateway;
+
+        return $this;
+    }
+
+    public function build(): Auth
+    {
+        return new Auth($this->authUserGateway);
+    }
+}
