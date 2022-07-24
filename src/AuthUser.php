c264004b0370f65e3e5e1c1d2344bfea728fd1aa
Chris Emerson <chris@cemerson.co.uk>
2022-07-24 22:22:36 +0100

    AuthUser object for obtaining user info

---
+++ src/AuthUser.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CEmerson\Auth;
+
+class AuthUser
+{
+    private string $id;
+    private array $userInfo;
+
+    public function __construct(string $id, array $userInfo)
+    {
+        $this->id = $id;
+        $this->userInfo = $userInfo;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function has(string $field): bool
+    {
+        return isset($this->userInfo[$field]);
+    }
+
+    public function get(string $field, ?string $default = null): ?string
+    {
+        if ($this->has($field)) {
+            return $this->userInfo[$field];
+        }
+
+        return $default;
+    }
+}
